7dbe04d6f75d649fa9ef4172ca5007d0b51cfae3
ipp203 <ipp203@freemail.hu>
2021-09-06 12:37:53 +0200

    GuinessApp init

---
+++ src/main/java/training360/guinessapp/GuinessappApplication.java
@@ -0,0 +1,13 @@
+package training360.guinessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuinessappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuinessappApplication.class, args);
+    }
+
+}
